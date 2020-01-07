c779d6a3c40103d8d9940a37f61d9c0971edcd56
Henry Hu <daydaybigking@sina.com>
2020-01-07 23:43:12 +0800

    把 build 的内容放出来

---
+++ precache-manifest.148a530835108009f2f53be26d829ac1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515710508e63d70e47f9d0e9feef24a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf0ddaeecae9ed49888d",
+    "url": "/static/css/main.07634d59.chunk.css"
+  },
+  {
+    "revision": "9c280a531e5650c8756d",
+    "url": "/static/js/2.51a94b56.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.51a94b56.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bf0ddaeecae9ed49888d",
+    "url": "/static/js/main.56556857.chunk.js"
+  },
+  {
+    "revision": "0e9f4bd9252d0c70f930",
+    "url": "/static/js/runtime-main.24e739df.js"
+  }
+]);
